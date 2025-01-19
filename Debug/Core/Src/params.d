196ea5df9d78ed27105de3d5ebb005846f02324c
CodeBlue-117 <jacob.price@nextgenrf.com>
2025-01-18 19:38:42 -0600

    Add STM32 Stepper Motor Project

---
+++ Debug/Core/Src/params.d
@@ -0,0 +1,7 @@
+Core/Src/params.o: ../Core/Src/params.c ../Core/Inc/params.h \
+ ../Core/Inc/L6470.h ../Core/Inc/microstepping_motor.h \
+ ../Core/Inc/xnucleoihm02a1.h
+../Core/Inc/params.h:
+../Core/Inc/L6470.h:
+../Core/Inc/microstepping_motor.h:
+../Core/Inc/xnucleoihm02a1.h:
