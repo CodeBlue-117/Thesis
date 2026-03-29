772fd42aece0573c9e0a6a4e667cb65fa438f281
CodeBlue-117 <jacob.price@nextgenrf.com>
2026-03-28 22:28:47 -0500

    Best operation yet

--- STEPPER_MOTOR_CONTROL_L6470/Core/Inc/l6470.h
+++ STEPPER_MOTOR_CONTROL_L6470/Core/Inc/l6470.h
@@ -22,12 +22,12 @@
 // Parameters
 #define MAX_ACCELERATION    			32  		// 10 pi rad/s = 32 rad/s = 5 rps/s // Used to be: 10  				/*!< max acceleration [rad/sec^2] 			*/
 #define MAX_SPEED_RAD    				31.4f 		// 10_PI rad/s = 31.4f rad/s = 5 rev/s		/*!< max speed [rad / sec] 					*/
-#define MAX_CURRENT         			1.0 				/*!< Max Current of the stepper motor 		*/
+#define MAX_CURRENT         			1.2 				/*!< Max Current of the stepper motor 		*/
 #define STEPS_PER_REVOLUTION  			200	                /*!< Steps per revolution 	*/
 #define TWOPI                       	6.283185f			/*!< Two pi value 			*/
 #define KVAL_HOLD_PERCENT           	5  // TODO: Optimize these
-#define KVAL_RUN_PERCENT            	85 // TODO: Optimize these --- lower values allow for smoother motor motion, higher values allow for larger torque
-#define KVAL_ACCDEC_PERCENT         	85 // TODO: Optimize these --- lower values allow for smoother motor motion, higher values allow for larger torque
+#define KVAL_RUN_PERCENT            	90 // TODO: Optimize these --- lower values allow for smoother motor motion, higher values allow for larger torque
+#define KVAL_ACCDEC_PERCENT         	90 // TODO: Optimize these --- lower values allow for smoother motor motion, higher values allow for larger torque
 #define DEFAULT_ZERO_POS				0
 
 // Microstepping Modes
--- STEPPER_MOTOR_CONTROL_L6470/Core/Src/main.c
+++ STEPPER_MOTOR_CONTROL_L6470/Core/Src/main.c
@@ -81,25 +81,25 @@ void l6470_sync_daisy_chain(MotorSetTypedef *stepper_motor);
 // TODO: Tune these PID parameters
 #define K_P_X 				35.0f // Proportional constant for x-dir
 #define K_P_Y 				35.0f // proportional constant for y-dir
-#define K_I_X 				5.0f //2.0f // 0.01f // Integral constant for x-dir
-#define K_I_Y 				5.0f // 2.0f //0.01f // Integral constant for y-dir
+#define K_I_X 				4.0f  //2.0f // 0.01f // Integral constant for x-dir
+#define K_I_Y 				4.0f  // 2.0f //0.01f // Integral constant for y-dir
 #define K_D_X 				0.0f  // 5.0f
 #define K_D_Y 				0.0f  // 5.0f
 
 // TODO: Tune the max vel
-#define MAX_CART_VEL 		0.50f //0.525f // 0.9f  // m/s, tune for safety (v = rw => v m/s = (0.03m) * (10)*PI = 0.94 m/s)
-#define MIN_CART_VEL 	   -0.50f // -0.525f //-0.9f
+#define MAX_CART_VEL 		0.60f //0.525f // 0.9f  // m/s, tune for safety (v = rw => v m/s = (0.03m) * (10)*PI = 0.94 m/s)
+#define MIN_CART_VEL 	   -0.60f // -0.525f //-0.9f
 
 // TODO: Tune the max integral???
-#define MAX_INTEGRAL  		5.0f // anti-windup cap on integral, tune
-#define MIN_INTEGRAL 	   -5.0f
+#define MAX_INTEGRAL  		6.5f // anti-windup cap on integral, tune
+#define MIN_INTEGRAL 	   -6.5f
 
 // TODO: Remove input POT filter until tested -> Model filter with random data and see what the output is.
 //#define POT_FC_HZ	  		15.0f // TODO: Tune this
 
 // TODO: TUNE the DEADBAND
-//#define DEADBAND 	  		(0.25f * M_PI/180.0f)  // 0.5 degree for the dead band (no integral)
-#define DEADBAND 	  		0.0f //(1.0f * M_PI/180.0f)  // 0.25 degree for the dead band (no integral)
+ #define DEADBAND 	  		(0.35f * M_PI/180.0f)  // 0.5 degree for the dead band (no integral)
+//#define DEADBAND 	  		(0.5f * M_PI/180.0f)  // 0.25 degree for the dead band (no integral)
 
 #define CONTROL_LOOP_TIME	100
 
